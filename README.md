3e9f3f637df672a85f4f18667b47d5fcaeb4cb5b
varshini_112 <varshinimuktha222@gmail.com>
2026-02-14 11:42:37 +0530

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Medical-Chatbot
+
+clone the repository 
+
+
+
+
+```bash
+Project repo: https://github.com/varshinimuktha/Medical-Chatbot.git
+```
+
+
+```bash
+conda create -n medibot python=3.10 -y
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Muktha Varshini",
+    author_email="varshinimuktha222@gmail.com",
+    #description="A complete chatbot built with LLMs,Langchain, pinecone, Flask and AWS",
+    pakages=find_packages(),
+    install_requires=[]
+)
