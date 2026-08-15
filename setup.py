f2057db434d84d06c22c9b62b8e49208f71196d6
varshini_112 <varshinimuktha222@gmail.com>
2026-08-15 11:32:02 +0530

    Committed

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     version="0.1.0",
     author="Muktha Varshini",
     author_email="varshinimuktha222@gmail.com",
-    #description="A complete chatbot built with LLMs,Langchain, pinecone, Flask and AWS",
-    pakages=find_packages(),
+    packages=find_packages(),
     install_requires=[]
 )
